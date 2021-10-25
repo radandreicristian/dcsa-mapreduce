2cfdc0f13a08801c2a2f239151bae102cf9a4669
Andrei-Cristian Rad <radandreicristian@gmail.com>
2021-10-25 12:13:01 +0200

    Update requirements. Add the text preprocessing utilities. Add the job script (w.i.p. - does not separate by genre)
    Update the job script to separate the words per genre.
    Add multi-lingual preposition removal.
    Add numbers and roman numbers removal.

--- src/movies/job.py
+++ src/movies/job.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from mrjob.job import MRJob
 from mrjob.step import MRStep
-from util.preprocessing import clear_text
+import util.preprocessing as pre
 
 
 class TopKeywordsJob(MRJob):
@@ -19,22 +19,25 @@ class TopKeywordsJob(MRJob):
         The keys are the words in the title (after preprocessing), and the values are 1's (no. of appearances).
         :param input_path: The location of the CSV
         :param _: The URI of the CSV (unused)
-        :return: The key-value pairs of words in the title and 1.
+        :return: Key-value tuples with with key=(word, genre) and value=1.
         """
         df = pd.read_csv(input_path)
         for _, data in df.iterrows():
             title = data['title']
-            clean_title = clear_text(title)
-            tokens = clean_title.split()
-            for token in tokens:
-                yield token, 1
+            clean_title = pre.preprocess_text(title)
+            title_words = clean_title.split()
+            genres = data['genres'].split("|")
+            for genre in genres:
+                for word in title_words:
+                    yield (word, genre), 1
 
     def combiner_sum(self, key, values):
         """
-        Combine the pairs with the same key (word) by adding their values. This step happens locally in the mapper node.
+        Combine the pairs with the same key=(word, genre) by adding their values.
+        This step happens locally in the mapper node.
 
-        :param key: A word.
-        :param values: The count of the word from the mapper (always "1").
+        :param key: (word, genre) tuples.
+        :param values: The count of the keys from the mapper (always "1").
         :return: Key-value pairs where the key is the word and the value is the node-local sum of the counts.
         """
         yield key, sum(values),
@@ -44,17 +47,17 @@ class TopKeywordsJob(MRJob):
         Combine the pairs with the same key (word) by adding their values (from all the mapper nodes).
         There is no need to have an effective key, so the value can become a tuple of (count, word).
 
-
         :param key: A word.
         :param values: The count of the word from the combiner.
         :return: Key-value pairs where the key is disregarded and the value is the tuple consisting of the word and
         """
-        yield None, (key, sum(values))
+        word, genre = key
+        yield genre, (word, sum(values))
 
-    def reducer_sort(self, _, values):
+    def reducer_sort(self, key, values):
         """
         Sort the tuples from the previous step (this is still a reducer) by their second element.
-        :param _: Keys (ignored, as the previous keys are None).
+        :param key: Keys (ignored, as the previous keys are None).
         :param values: The tuples consisting of the final count of the words' appearances.
         :return: The first maxWords words, by the number of appearances.
         """
@@ -62,7 +65,7 @@ class TopKeywordsJob(MRJob):
         for word, count in sorted(values,
                                   key=lambda x: x[1],
                                   reverse=True)[:top_size]:
-            yield word, count
+            yield key, (word, count)
 
     def steps(self):
         """
--- src/movies/util/preprocessing.py
+++ src/movies/util/preprocessing.py
@@ -1,7 +1,12 @@
+import re
 import string
 import nltk
 from nltk.corpus import stopwords
-import regex as re
+
+languages = ['english', 'italian', 'french']
+nltk.download('stopwords')
+languages_stopwords = [stopwords.words(language) for language in languages]
+all_stopwords = set([word for language_stopwords in languages_stopwords for word in language_stopwords])
 
 
 def remove_punctuation(text: str) -> str:
@@ -20,18 +25,36 @@ def remove_stop_words(text: str) -> str:
     :param text: A text string that may contain stop words, which we do not want for our ML model to process.
     :return: The text string without stop words.
     """
-    language_stopwords = set(stopwords.words('english'))
     text_tokens = text.split()
-    return " ".join([x for x in text_tokens if x not in language_stopwords])
+    return " ".join([x for x in text_tokens if x not in all_stopwords])
 
 
-def remove_years(text: str) -> str:
+def remove_numbers(text: str) -> str:
+    """
+    Remove the numbers from a string.
+
+    :param text: A string that may contain numbers.
+    :return: A string without numbers.
+    """
+    # From https://stackoverflow.com/a/68325680
     return text.translate(str.maketrans('', '', string.digits))
 
 
-def clear_text(text: str) -> str:
-    text = text.lower()
-    steps = [remove_years, remove_stop_words, remove_punctuation]
-    for step in steps:
-        text = step(text)
-    return text
+def remove_roman_numerals(text: str) -> str:
+    """
+    Remove the Roman numerals from a string.
+
+    :param text: A text that may contain roman numerals.
+    :return: A text without Roman numerals.
+    """
+    # From https://stackoverflow.com/a/68050802
+    pattern = r"\b(?=[mdclxvii])m{0,4}(cm|cd|d?c{0,3})(xc|xl|l?x{0,3})([ii]x|[ii]v|v?[ii]{0,3})\b\.?"
+    return re.sub(pattern, '', text, flags=re.I)
+
+
+def preprocess_text(text: str) -> str:
+    result = text.lower()
+    methods = [remove_numbers, remove_roman_numerals, remove_stop_words, remove_punctuation]
+    for method in methods:
+        result = method(result)
+    return result
